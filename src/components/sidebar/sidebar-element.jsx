233d2323c8eb3e7a686fb6b833f50b11228fe42b
curais <craisochoa22@gmail.com>
2020-05-25 02:24:50 -0600

    Fixed custom icon styling

--- src/components/sidebar/sidebar-element.jsx
+++ src/components/sidebar/sidebar-element.jsx
@@ -8,18 +8,13 @@ const SidebarElement = ({ children, text, path }) => {
 
     const selected = location.pathname.match(path);
 
-    const content = location.pathname.match('dashboard') && selected ?
-        React.cloneElement(children, {
-            style: { filter: 'invert(85%) sepia(43%) saturate(3521%) hue-rotate(352deg) brightness(102%) contrast(104%)' }
-        }) : children;
-
     return (
         <div className={`row selectable ${selected ? 'selected' : ''}`} style={{ padding: '10px' }}
             onClick={() => history.push(`/home/${path}`)}>
             <div className="col">
                 <div className="row align-items-center">
                     <div className="col-sm-1">
-                        {content}
+                        {children}
                     </div>
                     <div className="col">
                         <span className="h6" >{text}</span>
